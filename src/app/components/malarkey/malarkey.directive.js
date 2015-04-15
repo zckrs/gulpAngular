7351c6d983e2ef656bccae282ee38cd8876c5bec
Mehdy Dara <mdara@eleven-labs.com>
2015-04-15 15:59:10 +0200

    Directive restict to element

--- src/app/components/malarkey/malarkey.directive.js
+++ src/app/components/malarkey/malarkey.directive.js
@@ -8,27 +8,31 @@
 
   /** @ngInject */
   function malarkeyWrapper() {
-
-    return {
-      restrict: 'AE',
+    var directive = {
+      restrict: 'E',
       scope: {
         values: '=',
       },
       template: '<div></div>',
-      link: function(scope, element) {
-        var typist = malarkey(element[0], {
-          typeSpeed: 40,
-          deleteSpeed: 40,
-          pauseDelay: 800,
-          loop: true,
-          postfix: ' '
-        });
-
-        angular.forEach(scope.values, function(value) {
-          typist.type(value).pause().delete();
-        });
-      }
+      link: linkFunc
     };
+
+    return directive;
+
+    function linkFunc(scope, element) {
+      var typist = malarkey(element[0], {
+        typeSpeed: 40,
+        deleteSpeed: 40,
+        pauseDelay: 800,
+        loop: true,
+        postfix: ' '
+      });
+
+      angular.forEach(scope.values, function(value) {
+        typist.type(value).pause().delete();
+      });
+    }
+
   }
 
 })();

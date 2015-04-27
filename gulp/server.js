155a29e0c7ace82096b49cf757421da0d17d9df4
Mehdy Dara <mdara@eleven-labs.com>
2015-04-27 14:19:52 +0200

    Update with last master

--- gulp/server.js
+++ gulp/server.js
@@ -1,6 +1,9 @@
 'use strict';
 
+var path = require('path');
 var gulp = require('gulp');
+var conf = require('./conf');
+
 var browserSync = require('browser-sync');
 var browserSyncSpa = require('browser-sync-spa');
 
@@ -8,51 +11,48 @@ var util = require('util');
 
 var middleware = require('./proxy');
 
-module.exports = function(options) {
+function browserSyncInit(baseDir, browser) {
+  browser = browser === undefined ? 'default' : browser;
 
-  function browserSyncInit(baseDir, browser) {
-    browser = browser === undefined ? 'default' : browser;
-
-    var routes = null;
-    if(baseDir === options.src || (util.isArray(baseDir) && baseDir.indexOf(options.src) !== -1)) {
-      routes = {
-        '/bower_components': 'bower_components'
-      };
-    }
-
-    var server = {
-      baseDir: baseDir,
-      routes: routes
+  var routes = null;
+  if(baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
+    routes = {
+      '/bower_components': 'bower_components'
     };
-
-    if(middleware.length > 0) {
-      server.middleware = middleware;
-    }
-
-    browserSync.instance = browserSync.init({
-      startPath: '/',
-      server: server,
-      browser: browser
-    });
   }
 
-  browserSync.use(browserSyncSpa({
-    selector: '[ng-app]'// Only needed for angular apps
-  }));
+  var server = {
+    baseDir: baseDir,
+    routes: routes
+  };
 
-  gulp.task('serve', ['watch'], function () {
-    browserSyncInit([options.tmp + '/serve', options.src]);
-  });
+  if(middleware.length > 0) {
+    server.middleware = middleware;
+  }
 
-  gulp.task('serve:dist', ['build'], function () {
-    browserSyncInit(options.dist);
+  browserSync.instance = browserSync.init({
+    startPath: '/',
+    server: server,
+    browser: browser
   });
+}
 
-  gulp.task('serve:e2e', ['inject'], function () {
-    browserSyncInit([options.tmp + '/serve', options.src], []);
-  });
+browserSync.use(browserSyncSpa({
+  selector: '[ng-app]'// Only needed for angular apps
+}));
 
-  gulp.task('serve:e2e-dist', ['build'], function () {
-    browserSyncInit(options.dist, []);
-  });
-};
+gulp.task('serve', ['watch'], function () {
+  browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
+});
+
+gulp.task('serve:dist', ['build'], function () {
+  browserSyncInit(conf.paths.dist);
+});
+
+gulp.task('serve:e2e', ['inject'], function () {
+  browserSyncInit([conf.paths.tmp + '/serve', conf.paths.src], []);
+});
+
+gulp.task('serve:e2e-dist', ['build'], function () {
+  browserSyncInit(conf.paths.dist, []);
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,40 @@
 'use strict';
 
+var path = require('path');
+var conf = require('./gulp/conf');
+
+var _ = require('lodash');
+var wiredep = require('wiredep');
+
+function listFiles() {
+  var wiredepOptions = _.extend({}, conf.wiredep, {
+    dependencies: true,
+    devDependencies: true
+  });
+
+  return wiredep(wiredepOptions).js
+    .concat([
+      path.join(conf.paths.src, '/app/**/*.module.js'),
+      path.join(conf.paths.src, '/app/**/*.js'),
+      path.join(conf.paths.src, '/**/*.spec.js'),
+      path.join(conf.paths.src, '/**/*.mock.js')
+    ]);
+}
+
 module.exports = function(config) {
 
   var configuration = {
-    autoWatch : false,
+    files: listFiles(),
 
-    frameworks: ['jasmine'],
+    singleRun: true,
+
+    autoWatch: false,
+
+    frameworks: ['jasmine', 'angular-filesort'],
+
+    angularFilesort: {
+      whitelist: [path.join(conf.paths.src, '/**/!(*.html|*.spec|*.mock).js')]
+    },
 
     ngHtml2JsPreprocessor: {
       stripPrefix: 'src/',
@@ -16,6 +45,7 @@ module.exports = function(config) {
 
     plugins : [
       'karma-phantomjs-launcher',
+      'karma-angular-filesort',
       'karma-jasmine',
       'karma-ng-html2js-preprocessor'
     ],

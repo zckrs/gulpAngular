0d121bce60fd9b606af0979fe3c28732d1cad8c4
Mehdy Dara <mdara@eleven-labs.com>
2015-09-11 14:56:32 +0200

    Generate sample app with default option of generator-gulp-angular

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -11,7 +11,7 @@
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
-    vm.creationDate = 1441873165633;
+    vm.creationDate = 1441976142646;
     vm.showToastr = showToastr;
 
     activate();
